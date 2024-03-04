67788603b8c593e135969ea68a29e53bb187e747
Wani Alvan Seth <43110857+AlvanWani@users.noreply.github.com>
2024-03-04 16:11:25 +0200

    Add files via upload

---
+++ user_input.py
@@ -0,0 +1,7 @@
+name = input('Enter your Name: ')
+age = input('Enter your age: ')
+location = input('Enter your location:' )
+
+#print
+
+print('Hello', name + 'you are ', age + 'years old and leave in' +location)
